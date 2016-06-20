aa3d491f1f6699e19b1edf33fbf6c1483cc1c075
Flandre <192488668@qq.com>
2016-06-20 16:46:55 +0800

    init grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+  'use strict';
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+  // Set default file encoding utf-8.
+  grunt.file.defaultEncoding = 'utf-8';
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      react: {
+        src: 'node_modules/react/dist/react.min.js',
+        dest: 'lib/react.min.js'
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '.',
+          keepalive: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['copy', 'connect']);
+};
