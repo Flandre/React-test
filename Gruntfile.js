9e46604e5dd18db4c547fc3d499733b8f5aaa021
Flandre <192488668@qq.com>
2016-06-20 17:09:19 +0800

    add browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,10 @@ module.exports = function (grunt) {
       react_dom: {
         src: 'node_modules/react-dom/dist/react-dom.min.js',
         dest: 'lib/react-dom.min.js'
+      },
+      browser: {
+        src: 'node_modules/browser/browser.js',
+        dest: 'lib/browser.js'
       }
     },
     connect: {
