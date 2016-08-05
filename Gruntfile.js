43d5abe8591757bd2d1396b3a2475580f8c6f408
Flandre <flantan233@gmail.com>
2016-08-05 15:59:35 +0800

    config grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,5 +33,5 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['copy', 'connect']);
+  grunt.registerTask('default', ['copy']);
 };
